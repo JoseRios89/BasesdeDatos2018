9d99fc6983247763655c0809c353323fdd934457
jramscr <jramscr@gmail.com>
2018-07-28 14:52:06 -0600

    Agregar modelos basicos para el sistema

---
+++ src/sistema/modelos/Tiquete.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema.modelos;
+import java.util.Date;
+
+/**
+ *
+ * @author jramos
+ */
+public class Tiquete {
+    private String codigoTiquete;
+
+
+    private Date fechaCreacion;
+    private Date fechaSolucion;
+    private String descripcion;
+    private String solucion;
+    private String estado;
+
+    public Tiquete(String codigoTiquete, Date fechaCreacion, Date fechaSolucion, String descripcion, String solucion, String estado) {
+        this.codigoTiquete = codigoTiquete;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaSolucion = fechaSolucion;
+        this.descripcion = descripcion;
+        this.solucion = solucion;
+        this.estado = estado;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Date getFechaSolucion() {
+        return fechaSolucion;
+    }
+
+    public void setFechaSolucion(Date fechaSolucion) {
+        this.fechaSolucion = fechaSolucion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getSolucion() {
+        return solucion;
+    }
+
+    public void setSolucion(String solucion) {
+        this.solucion = solucion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }  
+
+    public String getCodigoTiquete() {
+        return codigoTiquete;
+    }
+
+    public void setCodigoTiquete(String codigoTiquete) {
+        this.codigoTiquete = codigoTiquete;
+    }
+}
